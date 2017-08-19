0f380884bac5fe224e664093f804e9c7a1c28db0
Alfaneos <alfaneos@gmail.com>
2017-08-19 11:23:27 +0600

    17

---
+++ include/AzureDBHandler.php
@@ -0,0 +1,10 @@
+<?php
+
+require '../vendor/autoload.php';
+class DbHandler
+{
+
+
+}
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once './include/Config.php';
 require './vendor/autoload.php';
+require_once  './include/AzureDBHandler.php';
 phpinfo();
 ?>
