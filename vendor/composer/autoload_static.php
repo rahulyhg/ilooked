9b0b4a188d460ab9965f94315ad09425f991db5b
Alfaneos <alfaneos@gmail.com>
2017-08-19 12:06:02 +0600

    26

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,32 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit63e89da26ab86b256fdbf5f8926ec230
 {
+    public static $prefixesPsr0 = array (
+        'P' => 
+        array (
+            'PEAR' => 
+            array (
+                0 => __DIR__ . '/..' . '/pear/pear_exception',
+            ),
+        ),
+        'H' => 
+        array (
+            'HTTP_Request2' => 
+            array (
+                0 => __DIR__ . '/..' . '/pear/http_request2',
+            ),
+        ),
+    );
+
+    public static $classMap = array (
+        'Net_URL2' => __DIR__ . '/..' . '/pear/net_url2/Net/URL2.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixesPsr0 = ComposerStaticInit63e89da26ab86b256fdbf5f8926ec230::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit63e89da26ab86b256fdbf5f8926ec230::$classMap;
 
         }, null, ClassLoader::class);
     }
